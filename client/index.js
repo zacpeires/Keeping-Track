f0807a5da7b97161b4327385e157796cc116574f
Zac Peires <zacpeires@googlemail.com>
2018-09-06 13:49:19 +0100

    set up: created user model and api, with functions for login and encrypton

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom'
+import '../public/index.css'
 import App from './store'
 import store from './store'
 
