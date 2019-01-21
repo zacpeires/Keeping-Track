319c97027b29d0d23ab67552c8040906e26b16df
Zac Peires <zacpeires@googlemail.com>
2019-01-21 11:26:08 +0300

    feature: basic navbar layout

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { logout } from '../store';
 import { LoginPage } from '../components'
+import { Link } from 'react-router-dom'
 
 class Navbar extends Component {
   constructor() {
@@ -13,9 +14,35 @@ class Navbar extends Component {
     return (
       <header>
         <nav className="header__navbar">
-          {this.props.isLoggedIn ? <div /> :
+        <div className="header__outer-navbar">
+        <h1>Keeping Track</h1>
+        <ul>
+          <li>
+            Home
+          </li>
+          <li>
+            About
+          </li>
+          <li>
+            Goals
+          </li>
+        </ul>
+        </div>
+        <div className="header__inner-navbar">
+        <ul>
+          {/* change Lis for ps, in order to animate height and color on hover - may have to use an after that comes up from the bottom.
+          UL becomes div in css and LI beomes p */}
+          <li>
+            Progress and Achievements
+          </li>
+          <li>
+            Tasks
+          </li>
+        </ul>
+        </div>
+          {/* {this.props.isLoggedIn ? <div /> :
           <div />
-        }
+        } */}
         </nav>
       </header>
     );
