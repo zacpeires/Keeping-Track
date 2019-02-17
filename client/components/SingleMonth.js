f470c2131120e1f01e8953a6c5744d12011abd26
Zachary Peires <zac@Zacharys-MacBook-Pro.local>
2019-02-17 15:15:47 +0000

    feature: dates are in ascending order

---
+++ client/components/SingleMonth.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+export default class SingleMonth extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+
+        return (
+            <div />
+        )
+    }
+}
--- client/components/WholeCalendar.js
+++ client/components/WholeCalendar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { gotAllDates, gotAllMonths } from "../store";
+import { SingleMonth } from "../components";
 
 class WholeCalendar extends Component {
   constructor() {
@@ -8,39 +9,74 @@ class WholeCalendar extends Component {
 
     this.state = {
       allMonths: [],
-      datesByMonths: []
+      January: [],
+      February: [],
+      March: [],
+      April: [],
+      May: [],
+      June: [],
+      July: [],
+      August: [],
+      September: [],
+      October: [],
+      November: [],
+      December: []
     };
 
-    this.sortDatesByMonth = this.sortDatesByMonth.bind(this)
+    this.sortDatesByMonth = this.sortDatesByMonth.bind(this);
+    this.alphabetizeDatesByMonth = this.alphabetizeDatesByMonth.bind(this);
   }
 
   async componentDidMount() {
     const { user } = this.props;
-    await this.props.gotAllMonths(user.id)
+    await this.props.gotAllMonths(user.id);
 
     this.setState({
       allMonths: this.props.months
     });
+
+    this.sortDatesByMonth();
   }
 
   sortDatesByMonth() {
-    
+    const allMonths = this.state.allMonths;
+    allMonths.forEach(month => {
+      const state = this.state;
+      for (let key in state) {
+        if (key === month.name) {
+       const sortedDates = this.alphabetizeDatesByMonth(month.dates)
+          this.setState({
+            [key]: sortedDates
+          });
+        }
+      }
+    });
   }
 
+  alphabetizeDatesByMonth(dates) {
+    return dates.sort((a, b) => {
+      return +a.date.split('/')[0] - +b.date.split('/')[0]
+    })
+
+    return datesInMonth
+  }
 
   render() {
-
-    if (!this.state.datesByMonthslength) {
-      this.sortDatesByMonth()
+    if (!this.state.allMonths) {
+      return <div />;
     }
-  
-    return <div />;
+
+    return (
+      <section className="whole-calendar">
+        {/* <SingleMonth /> */}
+      </section>
+    );
   }
 }
 
 const mapStateToProps = state => ({
   dates: state.dates,
-  months: state.month
+  months: state.months
 });
 const mapDispatchToProps = dispatch => ({
   gotAllDates: userId => dispatch(gotAllDates(userId)),
